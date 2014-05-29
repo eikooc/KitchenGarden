6f8626815473b486d50c01fc937d648a916a0c06
Jamie Neubert Pedersen <jamieneubert@gmail.com>
2014-05-29 13:16:56 +0200

    Add the controller

---
+++ chromeApp/js/app.js
@@ -0,0 +1,14 @@
+angular.module('kitchenGarden', ['ngRoute'])
+.controller('MainController', function($scope, $timeout) {
+  // Build the date object
+  $scope.date = {};
+
+  // Update function
+  var updateTime = function() {
+    $scope.date.raw = new Date();
+    $timeout(updateTime, 1000);
+  }
+
+  // Kick off the update function
+  updateTime();
+});
